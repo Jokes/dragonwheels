57d4c48fa1a20288ce4c14e2ddb1a1d7291f556e
Jokes <et.irrumabo@gmail.com>
2013-10-14 12:40:39 -0400

    Update colour-wheel.rkt

--- colour-wheel.rkt
+++ colour-wheel.rkt
@@ -1,7 +1,7 @@
 #lang racket/gui
 
 (require racket/block)
-(require "dragon-list.rkt")
+(require "dragon-list.rkt" "dragon-funcs.rkt" "dragon-colours.rkt")
 
 (define Nobody (Dragon "(nobody)" '(1 1 1)))
 (define pairing-male Nobody)
@@ -24,105 +24,6 @@
 (define vis '(#t #t #t))
 (define acc '(#t #t #f))
 (define accnum '(0 2 2))
-
-(define hex-values
-  '((aqua (114 196 196)) 
-    (avocado (86 124 52)) (azure (5 35 67)) (banana (253 255 114)) (beige (202 187 162)) 
-    (black (51 51 51)) (blood (69 15 15)) (blue (50 75 169)) (brown (117 81 54))
-    (caribbean (0 134 206)) (carmine (177 58 58)) (charcoal (84 84 84)) (chocolate (72 38 14))
-    (coal (75 73 70)) (coral (204 111 111)) (crimson (133 0 18)) (denim (47 69 87))
-    (emerald (32 96 63)) (fire (239 92 35)) (forest (66 80 53)) (gold (232 175 73))
-    (goldenrod (148 134 71)) (green (98 156 63)) (grey (126 126 126)) (ice (218 224 243))
-    (ivory (255 210 151)) (jade (97 171 137)) (jungle (30 54 26)) (lavender (204 164 224))
-    (leaf (165 227 45)) (lemon (255 230 59)) (magenta (233 52 170)) (maize (255 253 234))
-    (maroon (101 33 39)) (midnight (41 43 56)) (mulberry (110 35 93)) (navy (33 43 95))
-    (obsidian (0 0 0)) (orange (213 96 43)) (pink (231 127 191)) (platinum (200 190 206))
-    (purple (162 97 207)) (red (193 39 45)) (rose (255 214 246)) (royal (77 44 137))
-    (rust (139 50 32)) (sand (178 119 73)) (seafoam (170 241 177)) (shadow (58 46 68))
-    (silver (187 186 191)) (sky (174 200 255)) (slate (86 77 72)) (soil (90 69 52))
-    (splash (99 148 221)) (spring (169 160 50)) (steel (85 105 121)) (stone (150 145 130))
-    (stonewash (121 150 194)) (storm (117 122 219)) (sunshine (250 145 43)) (swamp (104 127 103))
-    (tangerine (255 115 96)) (teal (43 118 143)) (thistle (143 124 139)) (tomato (186 49 28))
-    (violet (100 63 156)) (white (255 255 255))))
-
-(define tum-values
-  '((chocolate (131 84 56)) 
-    (maize (230 219 163)) (shadow (73 57 57)) (sky (255 245 243))
-    (jungle (62 96 48)) (gold (244 188 127)) (rust (200 77 49)) (white (242 238 234))
-    (mulberry (140 76 203)) (stonewash (72 94 145)) (forest (39 50 29)) (sunshine (164 88 37))
-    (tomato (230 70 49)) (ice (192 195 224)) (thistle (97 75 88)) (steel (151 155 156))
-    (swamp (162 165 116)) (orange (238 136 70)) (crimson (81 15 21)) (platinum (249 249 249))
-    (lavender (136 96 181)) (denim (101 139 160)) (avocado (158 195 93)) (fire (165 64 53))
-    (blood (41 15 15)) (silver (94 94 105)) (purple (172 132 252)) (azure (50 139 201))
-    (green (103 180 124)) (tangerine (192 56 34)) (maroon (147 65 47)) (grey (167 167 167)) 
-    (violet (190 138 119)) (caribbean (0 70 109)) (leaf (100 146 12)) (sand (217 170 118))
-    (red (255 97 61)) (charcoal (143 143 143)) (royal (125 85 197)) (teal (69 199 198))
-    (spring (238 164 70)) (beige (114 108 86)) (carmine (111 40 60)) (coal (31 30 28))
-    (storm (66 68 99)) (aqua (132 231 193)) (goldenrod (186 141 38)) (stone (224 221 192))
-    (coral (246 196 165)) (black (201 192 190)) (navy (71 71 116)) (seafoam (221 250 205))
-    (lemon (159 112 57)) (slate (138 127 121)) (magenta (255 129 173)) (obsidian (47 54 56))
-    (blue (70 112 238)) (jade (51 124 90)) (banana (255 245 189)) (soil (136 118 93))
-    (pink (187 116 181)) (midnight (27 29 32)) (splash (41 59 127)) (emerald (111 123 63))
-    (ivory (255 219 177)) (brown (84 51 42)) (rose (247 159 201))))
-
-(define horn-values
-  '((chocolate (53 24 3))
-    (maize (177 173 60)) (shadow (67 52 54)) (sky (137 216 219)) (jungle (88 140 53))
-    (gold (241 198 158)) (rust (219 130 88)) (white (236 222 210)) (mulberry (84 56 83))
-    (stonewash (205 221 255)) (forest (135 155 117)) (sunshine (91 55 42)) (tomato (254 79 30))
-    (ice (174 184 214)) (thistle (216 197 230)) (steel (192 203 217)) (swamp (151 136 101))
-    (orange (218 171 127)) (crimson (36 15 16)) (platinum (67 67 67)) (lavender (99 68 119))
-    (denim (56 52 48)) (avocado (176 255 37)) (fire (91 51 47)) (blood (22 18 16))
-    (silver (233 233 233)) (purple (106 52 173)) (azure (48 154 240)) (green (71 68 93))
-    (tangerine (186 70 55)) (maroon (62 40 43)) (grey (190 190 190)) (violet (217 169 126))
-    (caribbean (0 40 63)) (leaf (32 82 44)) (sand (217 136 98)) (red (218 171 127))
-    (charcoal (36 36 36)) (royal (179 137 255)) (teal (50 61 73)) (spring (217 169 126))
-    (beige (66 56 18)) (carmine (44 27 32)) (coal (135 133 130)) (storm (49 43 68))
-    (aqua (198 238 225)) (goldenrod (170 129 0)) (stone (205 197 172)) (coral (245 171 166))
-    (black (193 193 193)) (navy (46 51 55)) (seafoam (103 193 141)) (lemon (83 68 0))
-    (slate (195 187 178)) (magenta (90 2 13)) (obsidian (47 54 56)) (blue (126 130 217))
-    (jade (67 67 67)) (banana (255 238 177)) (soil (48 26 8)) (pink (211 175 196)) 
-    (midnight (69 70 78)) (splash (95 226 222)) (emerald (193 217 91)) (ivory (255 250 243))
-    (brown (55 44 35)) (rose (255 255 255))))
-
-(define shade-values
-  '((maize (169 146 30))
-    (white (143 143 143)) (ice (76 92 122)) (platinum (95 72 98)) (silver (72 72 74))
-    (grey (51 51 51)) (charcoal (36 36 36))  (coal (31 30 28)) (black (11 11 11)) (obsidian (0 0 0))
-    (midnight (0 1 1)) (shadow (12 7 16)) (mulberry (55 14 37)) (thistle (60 41 49))
-    (lavender (60 41 49)) (purple (57 25 110)) (violet (36 29 73)) (royal (18 6 55))
-    (storm (23 26 71)) (navy (8 13 42)) (blue (12 25 74)) (splash (12 37 80)) (sky (34 68 138))
-    (stonewash (32 53 85)) (steel (31 46 60)) (denim (16 28 40)) (azure (3 26 48))
-    (caribbean (0 51 85)) (teal (29 39 52)) (aqua (25 82 84)) (seafoam (36 86 54)) (jade (11 77 61))
-    (emerald (5 50 29)) (jungle (3 16 1)) (forest (42 56 29)) (swamp (54 71 27))
-    (avocado (34 64 11)) (green (55 83 26)) (leaf (52 69 10)) (spring (76 85 21))
-    (goldenrod (112 75 18)) (lemon (93 57 24)) (banana (126 98 26)) (ivory (153 101 53))
-    (gold (103 58 0)) (sunshine (86 39 13)) (orange (99 61 11)) (fire (90 46 36))
-    (tangerine (110 27 15)) (sand (87 47 18)) (beige (121 104 79)) (stone (78 64 45))
-    (slate (47 37 28)) (soil (54 36 23)) (brown (43 32 24)) (chocolate (41 17 2)) (rust (82 26 15))
-    (tomato (84 17 6)) (crimson (79 5 0)) (blood (14 10 8)) (maroon (60 17 17)) (red (75 7 7))
-    (carmine (73 24 32)) (coral (134 60 60)) (magenta (128 22 72)) (pink (132 31 92))
-    (rose (222 114 175))))
-
-(define high-values
-  '((maize (254 252 231))
-    (white (252 252 252)) (ice (204 230 253)) (platinum (246 246 246)) (silver (231 231 231))
-    (grey (186 186 186)) (charcoal (175 175 175)) (coal (181 176 168)) (black (203 200 200))
-    (obsidian (120 123 124)) (midnight (120 120 172)) (shadow (148 124 162)) (mulberry (202 106 221))
-    (thistle (214 195 228)) (lavender (239 209 253)) (purple (221 175 253)) (violet (199 124 253))
-    (royal (190 151 251)) (storm (176 181 253)) (navy (85 95 238)) (blue (129 173 253))
-    (splash (93 224 220)) (sky (251 243 241)) (stonewash (203 219 253)) (steel (190 201 215))
-    (denim (102 181 208)) (azure (71 233 251)) (caribbean (46 244 253)) (teal (108 251 250))
-    (aqua (149 253 192)) (seafoam (156 249 137)) (jade (147 214 187)) (emerald (142 251 89))
-    (jungle (98 191 83)) (forest (138 179 95)) (swamp (200 221 161)) (avocado (175 253 80))
-    (green (185 253 156)) (leaf (206 253 117)) (spring (241 243 103)) (goldenrod (253 233 172))
-    (lemon (253 248 143)) (banana (253 253 212)) (ivory (253 242 228)) (gold (253 199 142))
-    (sunshine (253 199 104)) (orange (253 187 101)) (fire (251 155 83)) (tangerine (253 177 150))
-    (sand (253 206 154)) (beige (253 238 211)) (stone (221 217 188)) (slate (150 127 114))
-    (soil (163 141 109)) (brown (175 135 96)) (chocolate (182 121 73)) (rust (243 145 88))
-    (tomato (253 154 97)) (crimson (245 60 60)) (blood (173 52 52)) (maroon (215 89 77))
-    (red (253 123 89)) (carmine (253 133 133)) (coral (253 188 173)) (magenta (253 170 203))
-    (pink (252 189 228)) (rose (253 251 253))))
 
 (define hex-lookup
   (let ([hv (list (make-hash hex-values) (make-hash tum-values) (make-hash horn-values)
@@ -242,14 +143,13 @@
                          (name-dragon pairing-female) "-"
                          (number->string smaller-bound) ".png") 'png)))
 
-(define frame (new frame% [label "Dragonwheels v0.6"] [width 820] [height 600])) ; VERSION # HERE
+(define frame (new frame% [label "Dragonwheels v1.0"] ; VERSION NUMBER HERE
+                   [width 700] [height 500])) 
 (define horizon (new horizontal-panel% [parent frame] [alignment '(center center)] 
                      [min-height 100] [min-width 200] [style '(auto-vscroll auto-hscroll)]))
-(define left-p (new vertical-panel% [parent horizon] [stretchable-width #f]))
 (define mid-p (new vertical-panel% [parent horizon]))
 (define top-p (new horizontal-panel% [parent mid-p] [alignment '(center center)]
                    [stretchable-height #f]))
-(define right-p (new vertical-panel% [parent horizon] [stretchable-width #f]))
 
 (define my-canvas%
   (class canvas%
@@ -270,7 +170,8 @@
   (set! timon #f)
   (send pause-spin set-label "Spin")
   (send tim stop)
-  (set! acc '(#t #f #f))
+  (set! acc '(#t #t #f))
+  (set! accnum '(0 2 2))
   (for-each (λ (p n)
               (let ([b (first (send p get-children))] [b2 (second (send p get-children))])
                 (unless (equal? (substring (send b get-label) 0 4) "Hide")
@@ -293,9 +194,13 @@
 (define male-p (new vertical-panel% [parent (new horizontal-panel% [parent top-p] 
                                                  [alignment '(left center)])] 
                     [stretchable-width #f] [alignment '(right center)]))
-(define male-label (new message% [label ""] [auto-resize #t]
-                        [parent (new horizontal-panel% [parent male-p] [stretchable-height #f]
-                                     [alignment '(center center)])]))
+(define male-dragdrop 
+  (let ([m-dropdrags (cons Nobody male-dragons)])
+    (new choice% [label ""]
+         [parent (new horizontal-panel% [parent male-p] 
+                      [stretchable-height #f] [alignment '(center center)])]
+         [callback (λ (c e) (set-male (list-ref m-dropdrags (send c get-selection))))]
+         [choices (map name-dragon m-dropdrags)])))
 (define male-drops (colour-drops male-p get-male set-male))
 
 (define reset-b (new button% [parent top-p] [label "Reset"] [stretchable-height #t]
@@ -306,13 +211,14 @@
 (for-each 
  (λ (n s) 
    (define i-p (new horizontal-panel% [parent toggle-p] [stretchable-height #f]
-                    [alignment '(center center)]))
-   (new button% [parent i-p] [label (string-append "Hide " s)]
+                    [alignment '(right center)]))
+   (new message% [parent i-p] [label s])
+   (new button% [parent i-p] [label "Hide"]
         [callback (λ (b e) (set! vis (append (take vis n) (cons (not (list-ref vis n)) 
                                                                 (drop vis (add1 n)))))
                     (if (list-ref vis n)
-                        (send b set-label (string-append "Hide " s))
-                        (send b set-label (string-append "Show " s)))
+                        (send b set-label "Hide")
+                        (send b set-label "Show"))
                     (refresh-pairing))])
    (new button% [parent i-p] [stretchable-width #f]
         [label (string-append (if (list-ref acc n) "-" "+") "Accent")]
@@ -322,7 +228,7 @@
                         (send b set-label "-Accent") 
                         (send b set-label "+Accent"))
                     (refresh-pairing))]))
- '(0 1 2) '("Primary" "Secondary" "Tertiary"))
+ '(0 1 2) '("Primary:" "Secondary:" "Tertiary:"))
 
 (define pause-spin (new button% [parent top-p] [label "Spin"] [stretchable-height #t]
                         [callback (λ (b e) (if timon 
@@ -334,20 +240,23 @@
 (define female-p (new vertical-panel% [parent (new horizontal-panel% [parent top-p] 
                                                    [alignment '(right center)])] 
                       [stretchable-width #f] [alignment '(right center)]))
-(define female-label (new message% [label ""] [auto-resize #t]
-                          [parent (new horizontal-panel% [parent female-p] [stretchable-height #f]
-                                       [alignment '(center center)])]))
+(define female-dragdrop 
+  (let ([f-dropdrags (cons Nobody female-dragons)])
+    (new choice% [label ""]
+         [parent (new horizontal-panel% [parent female-p] 
+                      [stretchable-height #f] [alignment '(center center)])]
+         [callback (λ (c e) (set-female (list-ref f-dropdrags (send c get-selection))))]
+         [choices (map name-dragon f-dropdrags)])))
 (define female-drops
   (colour-drops female-p get-female set-female))
 
-(define export-b (new button% [parent mid-p] [label "Export"] [callback (λ (b e) (export-rings))]))
+(define export-b (new button% [parent toggle-p] [label "Export"] [stretchable-width #t]
+                      [callback (λ (b e) (export-rings))]))
 
 (define pairing-label (new message% [parent mid-p] [label ""] [auto-resize #t]))
 (define (refresh-pairing)
   (send pairing-label set-label (string-append (name-dragon pairing-male) "/" 
                                                (name-dragon pairing-female)))
-  (send male-label set-label (name-dragon pairing-male))
-  (send female-label set-label (name-dragon pairing-female))
   (send canvas refresh))
 (define canvas (new my-canvas% [parent mid-p] [min-width 100] [min-height 100]
                     [paint-callback 
@@ -355,12 +264,12 @@
                        (draw-rings dc))]))
 (define colour-label (new message% [parent mid-p] [label ""] [auto-resize #t]))
 
-(for-each (λ (d) (new button% [label (name-dragon d)] [parent left-p]
-                      [callback (λ (b e) (set-male d))])) 
-          (cons Nobody male-dragons))
-(for-each (λ (d) (new button% [label (name-dragon d)] [parent right-p]
-                      [callback (λ (b e) (set-female d))])) 
-          (cons Nobody female-dragons))
+#;(for-each (λ (d) (new button% [label (name-dragon d)] [parent left-p]
+                        [callback (λ (b e) (set-male d))])) 
+            (cons Nobody male-dragons))
+#;(for-each (λ (d) (new button% [label (name-dragon d)] [parent right-p]
+                        [callback (λ (b e) (set-female d))])) 
+            (cons Nobody female-dragons))
 
 (send frame show #t)
 (refresh-pairing)
